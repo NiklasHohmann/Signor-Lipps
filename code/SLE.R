1b352e73f4e122ac6c44de079c5c2a08f6dae9cb
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2026-09-26 12:44:52 +0200

    update Michals code, fix layout

--- code/SLE.R
+++ code/SLE.R
@@ -1,43 +1,58 @@
-# make some fake data
-x1 <- hist(c(rnorm(100), rnorm(100, 6)), plot=FALSE)$counts
-x2 <- c(6,6,6,6,6,6,10,10,10,6,6,6,6)
-x3 <- c(rnorm(10,1), rnorm(30,5), rnorm(10, 10), rnorm(50,20), rnorm(10,25))
-x4 <- c(10,10,10,6,6,6,2,2,2,6,6,20,20)
-x5 <- c(rnorm(100,1), rnorm(30,5), rnorm(10, 10), rnorm(5,20), rnorm(10,25))
-# assemble into lists (each represents one panel)
-p1 <- list(obs=x1, true=x2, prob=density(x3))
-p2 <- list(obs=x1, true=x4, prob=density(x5))
-# make a list of 12 panels
-x <- list(p1, p2, p1, p2, p1, p2, p1, p2, p1, p2, p1, p2)
-# setup the layout and margins
-np <- length(x)
-pannum <- rep(seq(1:(2*np)), time=rep(c(3,1),np))
-par(oma=c(2,2,1,1), mar=c(1,2,0,0))
-layout(matrix(pannum, 4, 12, byrow=T))
+load('data/fig_3.Rdata')
+extln <- split(df_lines, df_lines$panel)
+extbar <- split(df_lo, df_lo$panel)
+np <- length(extln)
 # plot panels
+pdf('figs/figure 3.pdf', width=4, height=6)
+pannum <- rep(seq(1:(2*np)), time=rep(c(2,1),np))
+matrix(pannum, 4, 9, byrow=T)
+par(oma=c(2,2,1,1), mar=c(1,1,0.5,0))
+layout(matrix(pannum, 4, 9, byrow=T))
+coltrue1 <- 'forestgreen' # color for true-extinctions line
+coltrue2 <- 'green' # color for true-extinctions background line
+borderobs <- 'gray40' # border color for observed extinctions
+colobs <- 'gray40' # bar color for observed extinctions
+colprob <- 'firebrick1' # fill color for sampling probability
+borderprob <- 'firebrick3' # border  color for sampling probability
 for (i in 1:np) {
-barplot(x[[i]][[1]], horiz = T, space = 0, 
-        col = 'black', axes = F)
-  points(x[[i]][[2]], 0:(length(x[[i]][[2]])-1),
-        type='l', col = 'skyblue2',lwd = 2, lend=2)
-  mtext(side=3, line=-1, adj=1.1, cex=0.8, LETTERS[i], xpd=NA)
-plot(x[[i]][[3]]$y, x[[i]][[3]]$x, xlab='', 
-     ylab='', type='n', axes=F)
-polygon(x[[i]][[3]]$y, x[[i]][[3]]$x,
-        col='firebrick1', border='firebrick')
+  h <- hist(extbar[[i]]$lo, plot=F, breaks = seq(0,3.2, length.out = 20))$counts
+  max(h)/max(extln[[i]]$ext)
+  if (i < 10) lnscalex <- max(h)/max(extln[[i]]$ext)
+  if (i > 9) lnscalex <- 0.2*max(h)/max(extln[[i]]$ext)
+  lnscaley <- length(h)/max(extln[[i]]$t)
+  densplot <- density(extln[[i]]$sampling_prob)
+  barplot(h, horiz = T, space = 0, col = colobs, 
+          border = borderobs, axes = F)
+  points(lnscalex*extln[[i]]$ext, lnscaley*extln[[i]]$t,
+         type='l', col = coltrue2,lwd = 4, lend=2, xpd=NA)
+  points(lnscalex*extln[[i]]$ext, lnscaley*extln[[i]]$t,
+         type='l', col = coltrue1,lwd = 2, lend=2, xpd=NA)
+  mtext(side=3, line=-0.6, adj=1.75, cex=0.8, LETTERS[i], xpd=NA)
+  if (i == 1) mtext(side=2, 'Sudden extinction', cex=0.7, line=0.5, col=coltrue1, xpd=NA)
+  if (i == 4) mtext(side=2, 'Pulsed extinction', cex=0.7, line=0.5, col=coltrue1, xpd=NA)
+  if (i == 7) mtext(side=2, 'Gradual extinction', cex=0.7, line=0.5, col=coltrue1, xpd=NA)
+  if (i == 10) mtext(side=2, 'Constant extinction', cex=0.7, line=0.5, col=coltrue1, xpd=NA)
+  if (i == 1) mtext(side=2, adj=0.3, line=-4, bquote("" %->% ""), cex=0.9)
+  if (i == 1) mtext(side=2, adj=0.3, line=-5, 'TIME', cex=0.7)
+  if (i == 10) mtext(side=1, adj=0.1, line=0.2, 'Ext. rate', cex=0.7, col=coltrue1)
+  if (i == 10) mtext(side=1, adj=0.1, line=-0.4, bquote("" %->% ""), cex=0.9, col=coltrue1)
+  plot(y = extln[[i]]$t,  x =  extln[[i]]$sampling_prob, xlab='', ylab='', type='n', axes=F, xlim = c(0,8))
+  polygon(y = c(0,extln[[i]]$t, 3.2),   x = c(0,extln[[i]]$sampling_prob,0), col = colprob, border= borderprob)
+  if (i == 10) mtext(side=1, adj=0.03, line=0.2, 'Prob.', cex=0.7, col=borderprob)
+  if (i == 10) mtext(side=1, adj=0.03, line=-0.4, bquote("" %->% ""), cex=0.9, col=borderprob)
 }
 # add boxes
 x_min <- 0.95*grconvertX(0, from = "ndc", to = "user")
-x_max <- 0.8*grconvertX(1, from = "ndc", to = "user")
+x_max <- 0.9*grconvertX(1, from = "ndc", to = "user")
 y_min <- 0.6*grconvertY(0, from = "ndc", to = "user")
 y_max <- 0.98*grconvertY(1, from = "ndc", to = "user")
 x_divide1 <- grconvertX(1.15/3, from = "ndc", to = "user")
-x_divide2 <- grconvertX(2.07/3, from = "ndc", to = "user")
-y_divide1 <- grconvertY(1.2/4, from = "ndc", to = "user")
-y_divide2 <- grconvertY(2.1/4, from = "ndc", to = "user")
-y_divide3 <- grconvertY(3.02/4, from = "ndc", to = "user")
-mylwd <- 1.5
-mygridcol <- 'darkgray'
+x_divide2 <- grconvertX(2.05/3, from = "ndc", to = "user")
+y_divide1 <- grconvertY(1.13/4, from = "ndc", to = "user")
+y_divide2 <- grconvertY(2.05/4, from = "ndc", to = "user")
+y_divide3 <- grconvertY(3.01/4, from = "ndc", to = "user")
+mylwd <- 1
+mygridcol <- 'black'
 segments(x_divide1, y_min, x_divide1, y_max, xpd=NA, col=mygridcol, lwd=mylwd)
 segments(x_divide2, y_min, x_divide2, y_max, xpd=NA, col=mygridcol, lwd=mylwd)
 segments(x_min, y_divide1, x_max, y_divide1, xpd=NA, col=mygridcol, lwd=mylwd)
@@ -45,3 +60,8 @@ segments(x_min, y_divide2, x_max, y_divide2, xpd=NA, col=mygridcol, lwd=mylwd)
 segments(x_min, y_divide3, x_max, y_divide3, xpd=NA, col=mygridcol, lwd=mylwd)
 rect(xleft = x_min, ybottom = y_min, xright = x_max, ytop = y_max, 
      border = "black", lwd = 1.5, xpd=NA)
+dev.off()
+
+
+
+
